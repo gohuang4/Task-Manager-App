d02ed66159f18af8360d2ec79c95eeae56b844a4
Gordon <gohuang4@gmail.com>
2022-06-17 17:55:22 -0700

    Feature 15 complete

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,8 @@ from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 from projects.models import Project
+from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -21,3 +23,15 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "projects/detail.html"
     context_object_name = "project_detail"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/new.html"
+    fields = ["name", "description", "members"]
+
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.projects = self.request.user
+        item.save()
+        return redirect("show_project")
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from tasks.models import Task
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+
 
 # Create your views here.
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "tasks/new.html"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.tasks = self.request.user
+        item.save()
+        return redirect("show_project")
